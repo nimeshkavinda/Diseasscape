133e1555ff0b76cb294508a30f8acfc510a2e211
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-03-07 15:27:38 +0530

    add colors to theme

---
+++ src/theme/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  primary: { bg: "#015EFF", text: "#fff" },
+  secondary: { bg: "#E5EBFF", text: "#015EFF" },
+  error: { primary: "#FF7A7D", secondary: "#FEE5E4" },
+  warning: "#FED37F",
+  success: { primary: "#70E28F", secondary: "#E1F9E8" },
+};
+
+export default colors;
