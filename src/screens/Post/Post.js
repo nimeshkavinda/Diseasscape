fb4afa239c02c115fc43183d477221f2db975e32
Nimesh Kavinda <nimeshkavinda13@gmail.com>
2022-03-31 16:18:28 +0530

    add post loading vid

--- src/screens/Post/Post.js
+++ src/screens/Post/Post.js
@@ -18,17 +18,15 @@ const Post = () => {
   const snapPoints = useMemo(() => ["32%"], []);
   return (
     <View style={styles.wrapper}>
-      {/* <Video
+      <Video
         style={styles.video}
-        source={{
-          uri: "https://firebasestorage.googleapis.com/v0/b/diseasscape.appspot.com/o/2.mp4?alt=media&token=d88374d3-afeb-4341-bf89-e53319a58f56",
-        }}
+        source={require("../../../assets/video/volunteer_vid.mp4")}
         useNativeControls={false}
         resizeMode="cover"
         isLooping
         isMuted
         shouldPlay
-      /> */}
+      />
       <BottomSheet
         ref={sheetRef}
         snapPoints={snapPoints}
